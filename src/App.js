e4f858548d8ad54171b6a0e2d9fa5fe809963550
Jaiten Thomas <jaiten.thomas@gmail.com>
2019-07-15 01:22:53 +1000

    Added loading indicator.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react';
 
-import Titles from "./components/Titles";
-import Form from "./components/Form";
-import Weather from "./components/Weather";
-import bg from "./img/bg.jpg";
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
+import ReactLoading from 'react-loading';
+import bg from './img/bg.jpg';
 
-const API_KEY = "259d3ca89dbc5eb363aba91fb17f92b7";
+const API_KEY = '259d3ca89dbc5eb363aba91fb17f92b7';
 
 class App extends React.Component {
   state = {
@@ -14,11 +15,13 @@ class App extends React.Component {
     country: undefined,
     humidity: undefined,
     description: undefined,
-    error: undefined
+    error: undefined,
+    loading: false
   };
 
   getWeather = async e => {
     e.preventDefault();
+    this.setState({ loading: true });
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     if (city && country) {
@@ -26,15 +29,23 @@ class App extends React.Component {
         `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=metric&appid=${API_KEY}`
       );
       const data = await api_call.json();
-      console.log(data);
-      this.setState({
-        temperature: data.main.temp,
-        city: data.name,
-        country: data.sys.country,
-        humidity: data.main.humidity,
-        description: data.weather[0].description,
-        error: ""
-      });
+
+      if (data.main === undefined) {
+        return this.setState({
+          error: 'Invalid City and/or Country.',
+          loading: false
+        });
+      } else {
+        this.setState({
+          temperature: data.main.temp,
+          city: data.name,
+          country: data.sys.country,
+          humidity: data.main.humidity,
+          description: data.weather[0].description,
+          error: '',
+          loading: false
+        });
+      }
     } else {
       this.setState({
         temperature: undefined,
@@ -42,10 +53,28 @@ class App extends React.Component {
         country: undefined,
         humidity: undefined,
         description: undefined,
-        error: "Please enter the values"
+        error: 'Please enter the values',
+        loading: false
       });
     }
   };
+
+  renderWeather() {
+    if (this.state.loading) {
+      return <ReactLoading type={'spin'} color={'red'} />;
+    }
+
+    return (
+      <Weather
+        temperature={this.state.temperature}
+        city={this.state.city}
+        country={this.state.country}
+        humidity={this.state.humidity}
+        description={this.state.description}
+        error={this.state.error}
+      />
+    );
+  }
 
   render() {
     return (
@@ -58,14 +87,7 @@ class App extends React.Component {
         <div className="box2-container">
           <div className="box2-content">
             <Form getWeather={this.getWeather} />
-            <Weather
-              temperature={this.state.temperature}
-              city={this.state.city}
-              country={this.state.country}
-              humidity={this.state.humidity}
-              description={this.state.description}
-              error={this.state.error}
-            />
+            {this.renderWeather()}
           </div>
         </div>
       </div>
